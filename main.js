e4f76d3d971db0f2d05f1a267ea16e5df27dce6b
Jhony Almeida <106479072+jhonleoni@users.noreply.github.com>
2024-10-20 15:58:14 -0400

    Add functionality to submit new image links and display them in the gallery

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
     $('form').on('submit', function(e) {
       e.preventDefault()
       const enderecoImagemNova = $('#endereco-imagem-nova').val()
-      
+      const novoItem = $('<li style="display: none"></li>')
+      $(`<img src="${enderecoImagemNova}" />`).appendTo(novoItem)
+      $(`
+        <div class="overlay-imagem-link">
+            <a href="${enderecoImagemNova}" target="_blank" title="Ver imagem em tamanho real" a>
+                Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn()
+        $('#endereco-imagem-nova').val('')
+
     })
 })
